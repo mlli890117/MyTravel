516d99f52b95b6d11a63baff0f41f25539c9fe1f
mlli890117 <88527319+mlli890117@users.noreply.github.com>
2026-08-17 13:31:56 +0800

    Refresh PWA cache strategy for app fix

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='my-travel-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='my-travel-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./fix.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
